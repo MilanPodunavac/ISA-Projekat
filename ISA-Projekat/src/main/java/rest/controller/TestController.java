f5961d157f68856e4fd7715f368fc4c945f010bb
Aleksandar <aleksandar.radisic@uns.ac.rs>
2021-11-27 00:01:03 +0100

    Initial project and test controller

---
+++ ISA-Projekat/src/main/java/rest/controller/TestController.java
@@ -0,0 +1,24 @@
+package rest.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping(value = "/getTest", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> test() {
+        return new ResponseEntity<String>("Test", HttpStatus.OK);
+    }
+}
