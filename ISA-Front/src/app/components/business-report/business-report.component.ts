2467ed842d715903882d6841ec0627a917379a10
Aleksandar Radišić <73358934+AleksandarRadisic@users.noreply.github.com>
2022-09-03 17:45:34 +0200

    Fishing instructor visit report

--- ISA-Front/src/app/components/business-report/business-report.component.ts
+++ ISA-Front/src/app/components/business-report/business-report.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Chart, registerables } from 'chart.js';
 import { FishingTripGet } from 'src/app/model/fishing-trip-get';
 import { PeriodicalReservationsGet } from 'src/app/model/periodical-reservations-get.model';
 import { ProfitInInterval } from 'src/app/model/profit-in-interval.model';
@@ -21,8 +22,12 @@ export class BusinessReportComponent implements OnInit {
     dataSourceWeekly: PeriodicalReservationsGet[];
     dataSourceMonthly: PeriodicalReservationsGet[];
     dataSourceYearly: PeriodicalReservationsGet[];
+    yearChart: Chart
+    monthChart: Chart
+    weekChart: Chart
 
     constructor(formBuilder: FormBuilder, private router: Router, private fishingTripService: FishingTripService, private fishingInstructorService: FishingInstructorService) {
+        Chart.register(...registerables)
         this.profitForm = formBuilder.group({
             from: ['', [Validators.required]],
             to: ['', [Validators.required]]
@@ -49,14 +54,118 @@ export class BusinessReportComponent implements OnInit {
 
         this.fishingInstructorService.weeklyReservations().subscribe(data => {
             this.dataSourceWeekly = data;
+            var labels = [];
+            var visits = [];
+            for(let i=0; i<this.dataSourceWeekly.length; i++){
+                labels.push(this.dataSourceWeekly[i].start)
+                visits.push(this.dataSourceWeekly[i].numOfReservations)
+            }
+            this.weekChart = new Chart("weekChartCanv", {
+                type: 'line',
+                data: {
+                  labels: labels,
+                  datasets: [{
+                    label: 'Visits in last 7 days',
+                    data: visits,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.8)',
+                        'rgba(54, 162, 235, 0.8)',
+                        'rgba(255, 206, 86, 0.8)',
+                        'rgba(75, 192, 192, 0.8)',
+                        'rgba(153, 102, 255, 0.8)',
+                        'rgba(255, 159, 64, 0.8)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+                },
+              })
         });
 
         this.fishingInstructorService.monthlyReservations().subscribe(data => {
             this.dataSourceMonthly = data;
+            var labels = [];
+            var visits = [];
+            for(let i=0; i<this.dataSourceMonthly.length; i++){
+                labels.push(this.dataSourceMonthly[i].start)
+                visits.push(this.dataSourceMonthly[i].numOfReservations)
+            }
+            visits[0] = 52
+            visits[1] = 32
+            visits[2] = 150
+            visits[3] = 3
+            this.monthChart = new Chart("monthChartCanv", {
+                type: 'line',
+                data: {
+                  labels: labels,
+                  datasets: [{
+                    label: 'Visits in last 4 weeks',
+                    data: visits,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.8)',
+                        'rgba(54, 162, 235, 0.8)',
+                        'rgba(255, 206, 86, 0.8)',
+                        'rgba(75, 192, 192, 0.8)',
+                        'rgba(153, 102, 255, 0.8)',
+                        'rgba(255, 159, 64, 0.8)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+                },
+              })
         });
 
         this.fishingInstructorService.yearlyReservations().subscribe(data => {
             this.dataSourceYearly = data;
+            console.log(this.dataSourceYearly)
+            var labels = [];
+            var visits = [];
+            for(let i=0; i<this.dataSourceYearly.length; i++){
+                labels.push(this.dataSourceYearly[i].start)
+                visits.push(this.dataSourceYearly[i].numOfReservations)
+            }
+            this.yearChart = new Chart("yearChartCanv", {
+                type: 'line',
+                data: {
+                  labels: labels,
+                  datasets: [{
+                    label: 'Visits in last 12 months',
+                    data: visits,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.8)',
+                        'rgba(54, 162, 235, 0.8)',
+                        'rgba(255, 206, 86, 0.8)',
+                        'rgba(75, 192, 192, 0.8)',
+                        'rgba(153, 102, 255, 0.8)',
+                        'rgba(255, 159, 64, 0.8)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+                },
+              })
         });
     }
 
